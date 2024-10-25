aefeb874db0792b96aa7e56f797daeb47eba42f7
Hamed Ghadirian <H.qadirian@gmail.com>
2024-10-25 22:42:55 +0200

    Initial Commit

---
+++ AppleCoder/AppleCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCoderApp.swift
+//  AppleCoder
+//
+//  Created by ghadirianh on 25.10.24.
+//
+
+import SwiftUI
+
+@main
+struct AppleCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleCoderTests/AppleCoderTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCoderTests.swift
+//  AppleCoderTests
+//
+//  Created by ghadirianh on 25.10.24.
+//
+
+import Testing
+@testable import AppleCoder
+
+struct AppleCoderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
